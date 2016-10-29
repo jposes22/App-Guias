655991e2f57a847a9da615402d71b28706701e22
jposes22 <jose@iMac.local>
2016-10-29 16:19:03 +0200

    Commit con Baronha funcionando correctamente e iconos de baroña y borneiro

---
+++ Codigo iOS/Plantilla Guias Turisticas/Borneiro/Utils/Constants/ConstantsURL.m
@@ -0,0 +1,19 @@
+//
+//  Constants.m
+//  TorresDeOeste
+//
+//  Created by Evelb on 8/10/16.
+//  Copyright © 2016 Evelb. All rights reserved.
+//
+
+#import "ConstantsURL.h"
+
+@implementation ConstantsURL
+
+///URL
+
+NSString * const kURL_SERVICE = @"http://95.123.56.172:8080/BORNEIRO/rest";
+
+
+
+@end

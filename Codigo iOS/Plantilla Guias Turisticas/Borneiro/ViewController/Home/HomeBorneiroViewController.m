1221ae4d28aa3600c84303189de7229a7a2254c6
jposes22 <evelb@MacBook-Pro-15.local>
2016-10-29 18:12:57 +0200

    Commit inestable

--- Codigo iOS/Plantilla Guias Turisticas/Borneiro/ViewController/Home/HomeBorneiroViewController.m
+++ Codigo iOS/Plantilla Guias Turisticas/Borneiro/ViewController/Home/HomeBorneiroViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "HomeBorneiroViewController.h"
-
+#import "UIViewController+MMDrawerController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+#import "UtilsAppearance.h"
+#import "Constants.h"
 @interface HomeBorneiroViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imagenBackground;
+@property (weak, nonatomic) IBOutlet UIButton *btnMenu;
+@property (weak, nonatomic) IBOutlet UIView *viewComoLlegar;
+@property (weak, nonatomic) IBOutlet UILabel *labelComoLlegar;
+@property (weak, nonatomic) IBOutlet UIView *viewCiudad;
+@property (weak, nonatomic) IBOutlet UILabel *labelCiudad;
+@property (weak, nonatomic) IBOutlet UIView *ViewVisita;
+@property (weak, nonatomic) IBOutlet UILabel *labelVisita;
+@property (weak, nonatomic) IBOutlet UIView *viewCultura;
+@property (weak, nonatomic) IBOutlet UILabel *labelCultura;
+@property (weak, nonatomic) IBOutlet UIView *viewSitiosInteres;
+@property (weak, nonatomic) IBOutlet UILabel *labelSitios;
+
 
 @end
 
@@ -17,12 +33,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self loadData];
+    [self loadStyle];
 }
-
+-(void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar setHidden:YES];
+}
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - load data
+-(void)loadData{
+    [_imagenBackground setContentMode:UIViewContentModeScaleAspectFill];
+
+   
+}
+-(void)loadStyle{
+    self.labelComoLlegar.adjustsFontSizeToFitWidth = YES;
+    self.labelCiudad.adjustsFontSizeToFitWidth = YES;
+    self.labelSitios.adjustsFontSizeToFitWidth = YES;
+    self.labelVisita.adjustsFontSizeToFitWidth = YES;
+    self.labelCultura.adjustsFontSizeToFitWidth = YES;
+    
+    UITapGestureRecognizer * gestureComollegar =  [[UITapGestureRecognizer alloc] initWithTarget:self  action:@selector(tapComoLlegar:)];
+    gestureComollegar.numberOfTapsRequired = 1;
+    [self.viewComoLlegar addGestureRecognizer:gestureComollegar];
+    UITapGestureRecognizer * gestureCiudad =  [[UITapGestureRecognizer alloc] initWithTarget:self  action:@selector(tapCiudad:)];
+    gestureCiudad.numberOfTapsRequired = 1;
+    [self.viewCiudad addGestureRecognizer:gestureCiudad];
+    UITapGestureRecognizer * gestureVisita =  [[UITapGestureRecognizer alloc] initWithTarget:self  action:@selector(tapComenzar:)];
+    gestureVisita.numberOfTapsRequired = 1;
+    [self.ViewVisita addGestureRecognizer:gestureVisita];
+    UITapGestureRecognizer * gestureCultura =  [[UITapGestureRecognizer alloc] initWithTarget:self  action:@selector(tapCultura:)];
+    gestureCultura.numberOfTapsRequired = 1;
+    [self.viewCultura addGestureRecognizer:gestureVisita];
+    UITapGestureRecognizer * gestureSitios =  [[UITapGestureRecognizer alloc] initWithTarget:self  action:@selector(tapSitios:)];
+    gestureSitios.numberOfTapsRequired = 1;
+    [self.viewSitiosInteres addGestureRecognizer:gestureSitios];
+ 
+}
+#pragma mark - Me
 
 /*
 #pragma mark - Navigation
@@ -33,5 +88,22 @@
     // Pass the selected object to the new view controller.
 }
 */
-
+- (IBAction)btnMenuTouch {
+     [self.mm_drawerController toggleDrawerSide:MMDrawerSideRight animated:YES completion:nil];
+}
+- (void)tapComoLlegar:(UITapGestureRecognizer *)tap{
+    
+}
+- (void)tapCiudad:(UITapGestureRecognizer *)tap{
+    
+}
+- (void)tapComenzar:(UITapGestureRecognizer *)tap{
+    
+}
+- (void)tapCultura:(UITapGestureRecognizer *)tap{
+    
+}
+- (void)tapSitios:(UITapGestureRecognizer *)tap{
+    
+}
 @end

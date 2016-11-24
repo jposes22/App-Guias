b904495248cd808dc3ba7ae3c9b32a6c105b031f
jposes22 <evelb@MacBook-Pro-15.local>
2016-11-24 20:53:50 +0100

    Modificación celdas

--- Codigo iOS/Plantilla Guias Turisticas/Borneiro/ViewController/ACidaBorneiro/View/Cell/cellCidaBorneiro.m
+++ Codigo iOS/Plantilla Guias Turisticas/Borneiro/ViewController/ACidaBorneiro/View/Cell/cellCidaBorneiro.m
@@ -90,8 +90,8 @@
         tapSaberMasGesture.numberOfTapsRequired = 1;
         [_labelSaberMas addGestureRecognizer:tapSaberMasGesture];
     }else{
-        _constrainHeightSaberMas.constant = -5;
-        _labelSaberMas.hidden = YES;
+        _constrainHeightSaberMas.constant = 0;
+        //_labelSaberMas.hidden = YES;
         _imageSaberMas.hidden = YES;
     }
 
